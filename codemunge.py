c309bd107ce8057acd8e130da20bbbadb1fa50d4
Shane Hathaway <shane@hathawaymix.org>
2013-05-03 18:10:51 -0600

    More flesh. Needs the actual code generation now.

--- codemunge.py
+++ codemunge.py
@@ -4,12 +4,13 @@ import weakref
 
 
 decl_re = re.compile(r'(\s*)(class|def)\s+([A-Za-z_][A-Za-z0-9_]*)')
-other_code_re = re.compile(r'(\s*)(#)?\S+')
+other_code_re = re.compile(r'(\s*)(#)?\S')
 tab_replacement = ' ' * 8
 
 
 class Decl(object):
-
+    """A declaration statement. Usually starts with 'def' or 'class'.
+    """
     def __init__(self, indent, name, first_row, last_row=None, children=None):
         self.indent = indent
         self.name = name
@@ -19,7 +20,7 @@ class Decl(object):
         self.parent_ref = None  # A weakref.ref
 
     def __repr__(self):
-        return ('{}({!r}, {!r}, {!r}, {!r}, {!r})'
+        return ('{0}({1!r}, {2!r}, {3!r}, {4!r}, {5!r})'
                 .format(self.__class__.__name__,
                         self.indent,
                         self.name,
@@ -49,10 +50,12 @@ def measure_indent(indent_chars):
 
 
 def list_decls(content):
+    """List the nested declarations in a module."""
     top = ModuleDecl(-1, '', 0)
     prev = top
 
-    for row, line in enumerate(content.split('\n')):
+    lines = content.split('\n')
+    for row, line in enumerate(lines):
         match = decl_re.match(line)
         if match is not None:
             indent_chars, decl_type, name = match.groups()
@@ -71,7 +74,7 @@ def list_decls(content):
                     indent = measure_indent(indent_chars)
                     prev = close_decls(prev, indent, row)
 
-    close_decls(prev, 0, row + 1)
+    close_decls(prev, 0, len(lines))
     return top.children
 
 
@@ -84,6 +87,17 @@ def close_decls(decl, indent, row):
     return decl
 
 
+def find_decl_for_row(decls, row):
+    for decl in decls:
+        if row >= decl.first_row and row <= decl.last_row:
+            child = find_decl_for_row(decl.children, row)
+            if child is not None:
+                return child
+            else:
+                return decl
+    return None
+
+
 def test_list_decls():
     content = ("if 1:\n"
                " def foo():\n"
@@ -91,6 +105,7 @@ def test_list_decls():
                " class bar():\n"
                "  def baz():\n"
                "   pass\n"
+               " # hi!\n"
                "  stop = True\n"
                "class Y: pass\n"
                "class Z:\n"
@@ -99,12 +114,26 @@ def test_list_decls():
     assert len(decls) == 4
     assert decls[0].name == 'foo'
     assert len(decls[0].children) == 0
+    assert decls[0].first_row == 1
+    assert decls[0].last_row == 2
+
     assert decls[1].name == 'bar'
     assert len(decls[1].children) == 1
+    assert decls[1].first_row == 3
+    assert decls[1].last_row == 5
     assert decls[1].children[0].name == 'baz'
+    assert decls[1].children[0].first_row == 4
+    assert decls[1].children[0].last_row == 5
+
     assert decls[2].name == 'Y'
+    assert len(decls[2].children) == 0
+    assert decls[2].first_row == 8
+    assert decls[2].last_row == 8
+
     assert decls[3].name == 'Z'
-    print decls
+    assert len(decls[3].children) == 0
+    assert decls[3].first_row == 9
+    assert decls[3].last_row == 10
 
 
 if __name__ == '__main__':
--- toggletest.py
+++ toggletest.py
@@ -1,16 +1,20 @@
 
 import codemunge
 import os
+import re
 import sublime
 import sublime_plugin
 
 
-class ToggleTestCommand(sublime_plugin.TextCommand):
+_builders = {}  # {filename: CodeBuilder}
 
+
+class ToggleTestCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        fname = self.view.file_name()
+        view = self.view
+        fname = view.file_name()
         if not fname:
-            sublime.status_message("toggle_test: No file name given")
+            sublime.status_message("toggle_test: No file name given.")
             return
 
         dirname, basename = os.path.split(fname)
@@ -19,18 +23,18 @@ class ToggleTestCommand(sublime_plugin.TextCommand):
             sublime.status_message("toggle_test: for .py files only.")
             return
 
-        content = self.view.substr(0)
-        point = self.view.sel()[0].begin()
-        row, _col = self.view.rowcol(point)
+        content = view.substr(sublime.Region(0, view.size()))
+        point = view.sel()[0].begin()
+        row, _col = view.rowcol(point)
 
         if os.path.basename(dirname) == 'tests':
             if basename.startswith('test_'):
                 # The file is a test. Go to the main code.
                 parent = os.path.dirname(dirname)
                 target = os.path.join(parent, basename[5:])
-                target_row = prepare_main_code(content, row, target)
+                builder = MainCodeBuilder(content, row)
             else:
-                sublime.status_message("toggle_test: {0} is not a test module"
+                sublime.status_message("toggle_test: {0} is not a test module."
                                        .format(fname))
                 return
         else:
@@ -46,15 +50,154 @@ class ToggleTestCommand(sublime_plugin.TextCommand):
                 f = open(init_py, 'w')
                 f.close()
 
-            target_row = prepare_test_code(content, row, target)
+            builder = TestCodeBuilder(content, row)
 
-        win = self.view.window()
-        win.open_file(target, target_row)
+        win = view.window()
+        view = win.open_file(target)
+
+        if view.is_loading():
+            _builders[os.path.abspath(target)] = builder
+        else:
+            builder.goto_target(view)
 
 
-def prepare_test_code(main_content, main_row, test_file):
-    return 0
+class Listener(sublime_plugin.EventListener):
+    def on_load(self, view):
+        fn = os.path.abspath(view.file_name())
+        builder = _builders.get(fn)
+        if builder is not None:
+            del _builders[fn]
+            builder.goto_target(view)
 
 
-def prepare_main_code(test_content, test_row, main_file):
-    return 0
+def get_decl_map(view):
+    content = view.substr(sublime.Region(0, view.size()))
+    decls = codemunge.list_decls(content)
+    return dict((decl.name, decl) for decl in decls)
+
+
+def to_test_name(name):
+    if name[:1].upper() == name[:1]:
+        return 'Test{0}'.format(name)
+    else:
+        return 'Test_{0}'.format(name)
+
+
+def to_main_name(name):
+    if name.startswith('Test_'):
+        return name[5:]
+    elif name.startswith('Test'):
+        return name[4:]
+    else:
+        return None
+
+
+def show_row(view, row):
+    point = view.text_point(row, 0)
+    line = view.substr(view.line(point))
+    match = re.match(r'\s+', line)
+    if match is not None:
+        point += len(match.group(0))
+    view.sel().clear()
+    view.sel().add(sublime.Region(point))
+    view.show(point)
+
+
+class CodeBuilder(object):
+    def __init__(self, content, row):
+        self.source_decls = codemunge.list_decls(content)
+        self.source_decl = codemunge.find_decl_for_row(self.source_decls, row)
+
+    def lineage(self):
+        decl = self.source_decl
+        while decl is not None and not isinstance(decl, codemunge.ModuleDecl):
+            yield decl
+            decl = decl.parent_ref()
+
+    def get_correlated_row(self, target_view, source_name, convert_name):
+        """Get the row in the target view that correlates with the source name.
+
+        convert_name is a function that converts a source_name to
+        the correlated target_name.
+
+        Returns (row, found). When not found, returns the row where it should
+        have been found.
+        """
+        target_decl_map = get_decl_map(target_view)
+        target_name = convert_name(source_name)
+        decl = target_decl_map.get(target_name)
+        if decl is not None:
+            return decl.first_row + 1, True
+
+        # The target code does not exist.
+        # Figure out where the new code belongs in the file.
+        row = 0
+        for source_decl in self.source_decls:
+            if source_decl.name == source_name:
+                break
+            target_decl = target_decl_map.get(convert_name(source_decl.name))
+            if target_decl is not None:
+                # The new code belongs somewhere after this code.
+                row = max(row, target_decl.last_row)
+
+        if not row:
+            # Place the new code where the first block of code already is.
+            row = min(decl.first_row for decl in target_decl_map.itervalues())
+
+        return row, False
+
+
+class TestCodeBuilder(CodeBuilder):
+    """Build and navigate to test code."""
+
+    def goto_target(self, target_view):
+        decls = list(self.lineage())
+        decls.reverse()
+        if not decls:
+            # No particular declaration was specified.
+            return
+
+        if isinstance(decls[0], codemunge.ClassDecl):
+            if len(decls) >= 2 and isinstance(decls[1], codemunge.FuncDecl):
+                self.goto_method(target_view, decls[0].name, decls[1].name)
+            else:
+                self.goto_class(target_view, decls[0].name)
+        elif isinstance(decls[0], codemunge.FuncDecl):
+            self.goto_func(target_view, decls[0].name)
+
+    def goto_class(self, target_view, class_name):
+        sublime.status_message("toggle_test: goto_class({0!r})"
+                               .format(class_name))
+
+        row, found = self.get_correlated_row(target_view,
+                                             class_name,
+                                             to_test_name)
+
+        if not found:
+            # TODO: generate the test code.
+            pass
+
+        show_row(target_view, row)
+
+    def goto_method(self, target_view, class_name, method_name):
+        sublime.status_message("toggle_test: goto_method({0!r}, {1!r})"
+                               .format(class_name, method_name))
+
+    def goto_func(self, target_view, func_name):
+        sublime.status_message("toggle_test: goto_func({0!r})"
+                               .format(func_name))
+
+        row, found = self.get_correlated_row(target_view,
+                                             func_name,
+                                             to_test_name)
+
+        if not found:
+            # TODO: generate the test code.
+            pass
+
+        show_row(target_view, row)
+
+
+class MainCodeBuilder(CodeBuilder):
+    def goto_target(self, target_view):
+        pass
