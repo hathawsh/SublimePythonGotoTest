ad8c07daf4c87cf13dee6df669c8bc28d9150a45
Shane Hathaway <shane@hathawaymix.org>
2013-05-06 13:07:45 -0600

    Implement two commands rather than one: goto_test and generate_test

--- toggletest.py
+++ toggletest.py
@@ -5,7 +5,7 @@ import re
 import sublime
 import sublime_plugin
 
-_builders = {}  # {filename: CodeBuilder}
+_deferred = {}  # {filename: CodeNav}
 
 
 class BasicTestGenerator(object):
@@ -49,7 +49,9 @@ class {testname}(unittest.TestCase):
         return self.class_test_template.format(**template_vars)
 
 
-class ToggleTestCommand(sublime_plugin.TextCommand):
+class GotoTestCommand(sublime_plugin.TextCommand):
+    generate = False
+
     def run(self, edit):
         view = self.view
         fname = view.file_name()
@@ -72,26 +74,21 @@ class ToggleTestCommand(sublime_plugin.TextCommand):
                 # The file is a test. Go to the main code.
                 parent = os.path.dirname(dirname)
                 target = os.path.join(parent, basename[5:])
-                builder = MainCodeBuilder(target, content, row)
+                nav = MainCodeNavigator(target, content, row)
             else:
                 sublime.status_message("toggle_test: {0} is not a test module."
                                        .format(fname))
                 return
         else:
             # The file is the main code. Go to the test code.
-            builder = TestCodeBuilder(fname, content, row)
+            nav = TestCodeNavigator(fname, content, row,
+                                    generate=self.generate)
 
             parent = os.path.join(dirname, 'tests')
             test_fn = 'test_' + basename
             target = os.path.join(parent, test_fn)
             if not os.path.exists(parent):
                 os.mkdir(parent)
-
-            if not os.path.exists(target):
-                head = builder.testgen.make_test_head(builder.template_vars)
-                f = open(target, 'w')
-                f.write(head)
-                f.close()
 
             init_py = os.path.join(parent, '__init__.py')
             if not os.path.exists(init_py):
@@ -103,18 +100,22 @@ class ToggleTestCommand(sublime_plugin.TextCommand):
         view = win.open_file(target)
 
         if view.is_loading():
-            _builders[os.path.abspath(target)] = builder
+            _deferred[os.path.abspath(target)] = nav
         else:
-            builder.goto_target(view)
+            nav.goto(view)
+
+
+class GenerateTestCommand(GotoTestCommand):
+    generate = True
 
 
 class Listener(sublime_plugin.EventListener):
     def on_load(self, view):
         fn = os.path.abspath(view.file_name())
-        builder = _builders.get(fn)
-        if builder is not None:
-            del _builders[fn]
-            builder.goto_target(view)
+        nav = _deferred.get(fn)
+        if nav is not None:
+            del _deferred[fn]
+            nav.goto(view)
 
 
 def list_view_decls(view):
@@ -154,8 +155,36 @@ def show_row(view, row):
     view.show(point)
 
 
-def insert_rows(view, row, content):
+def insert_rows(view, row, content, margin=2):
     point = view.text_point(row, 0)
+
+    if margin:
+        if row > 0:
+            # Add blank lines before.
+            region = sublime.Region(max(0, point - margin), point)
+            text_before = view.substr(region)
+            blanks = 0
+            for c in reversed(text_before):
+                if c == '\n':
+                    blanks += 1
+                else:
+                    break
+            if blanks < margin:
+                content = '\n' * (margin - blanks) + content
+
+        if point < view.size() - 1:
+            # Add blank lines after.
+            region = sublime.Region(point, min(view.size(), point + margin))
+            text_after = view.substr(region)
+            blanks = 0
+            for c in text_after:
+                if c == '\n':
+                    blanks += 1
+                else:
+                    break
+            if blanks < margin:
+                content = content + '\n' * (margin - blanks)
+
     edit = view.begin_edit('insert_test')
     view.insert(edit, point, content)
     view.end_edit(edit)
@@ -165,11 +194,11 @@ def insert_rows(view, row, content):
     view.show(region)
 
 
-class CodeBuilder(object):
-    def __init__(self, filename, content, row):
+class CodeNavigator(object):
+    def __init__(self, filename, content, source_row):
         self.source_decls = codemunge.list_decls(content)
-        self.source_decl = codemunge.find_decl_for_row(self.source_decls, row)
-        self.testgen = BasicTestGenerator()
+        self.source_decl = codemunge.find_decl_for_row(self.source_decls,
+                                                       source_row)
 
         basename = os.path.basename(filename)
         relmodule, _ext = os.path.splitext(basename)
@@ -209,10 +238,15 @@ class CodeBuilder(object):
         return None, row
 
 
-class TestCodeBuilder(CodeBuilder):
-    """Build and navigate to test code."""
+class TestCodeNavigator(CodeNavigator):
+    """Navigate to test code and optionally generate it."""
 
-    def goto_target(self, target_view):
+    def __init__(self, filename, content, row, generate):
+        super(TestCodeNavigator, self).__init__(filename, content, row)
+        self.testgen = BasicTestGenerator()
+        self.generate = generate
+
+    def goto(self, target_view):
         decls = self.source_decl.get_path()
         if not decls:
             # No particular declaration was specified.
@@ -221,6 +255,10 @@ class TestCodeBuilder(CodeBuilder):
         name = decls[0].name
         self.template_vars['name'] = name
         self.template_vars['testname'] = to_test_class_name(name)
+
+        if self.generate and not target_view.size():
+            content = self.testgen.make_test_head(self.template_vars)
+            insert_rows(target_view, 0, content)
 
         if isinstance(decls[0], codemunge.ClassDecl):
             if len(decls) >= 2 and isinstance(decls[1], codemunge.FuncDecl):
@@ -238,7 +276,7 @@ class TestCodeBuilder(CodeBuilder):
                                              class_decl.name,
                                              to_test_class_name)
 
-        if not target_decl is None:
+        if target_decl is None and self.generate:
             content = self.testgen.make_class_test(self.template_vars)
             insert_rows(target_view, row, content)
         else:
@@ -252,7 +290,7 @@ class TestCodeBuilder(CodeBuilder):
                                              func_decl.name,
                                              to_test_class_name)
 
-        if target_decl is None:
+        if target_decl is None and self.generate:
             content = self.testgen.make_function_test(self.template_vars)
             insert_rows(target_view, row, content)
         else:
@@ -266,7 +304,7 @@ class TestCodeBuilder(CodeBuilder):
                                                    class_decl.name,
                                                    to_test_class_name)
 
-        if target_class_decl is None:
+        if target_class_decl is None and self.generate:
             content = self.testgen.make_class_test(self.template_vars)
             insert_rows(target_view, row, content)
 
@@ -281,17 +319,17 @@ class TestCodeBuilder(CodeBuilder):
                                        to_test_method_name)
             target_method_decl, row, found = tup
 
-            if target_method_decl is None:
+            if target_method_decl is None and self.generate:
                 template_vars = {}
                 template_vars.update(template_vars)
                 template_vars['name'] = method_decl.name
                 template_vars['method'] = to_test_method_name(method_decl.name)
                 content = self.testgen.make_method_test(template_vars)
-                insert_rows(target_view, row, content)
+                insert_rows(target_view, row, content, margin=1)
 
         show_row(target_view, row)
 
 
-class MainCodeBuilder(CodeBuilder):
-    def goto_target(self, target_view):
+class MainCodeNavigator(CodeNavigator):
+    def goto(self, target_view):
         pass
