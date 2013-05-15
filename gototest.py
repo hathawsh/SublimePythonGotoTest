2de1d78f1ee400a931ae7358af2e250a4bf76835
Shane Hathaway <shane@hathawaymix.org>
2013-05-14 18:49:35 -0600

    Imports among Sublime plugins seem to be fairly broken. Mixed the modules into one.

--- gototest.py
+++ gototest.py
@@ -1,10 +1,135 @@
 
-import codemunge
+import ast
 import os
+import re
 import sublime
 import sublime_plugin
+import weakref
+
+empty_line_re = re.compile(r'\s*$')
 
 _deferred = {}  # {filename: CodeNav}
+
+
+class Decl(object):
+    """A function or class declaration statement."""
+    def __init__(self, name, first_row, last_row=None, children=None):
+        self.name = name
+        self.first_row = first_row
+        # Note: last_row includes the blank rows after the declaration.
+        self.last_row = last_row
+        self.children = children or []
+        self.parent_ref = None  # A weakref.ref
+
+    def get_path(self):
+        path = []
+        decl = self
+        while decl is not None:
+            path.append(decl)
+            parent = decl.parent_ref()
+            if parent is None or isinstance(parent, ModuleDecl):
+                break
+            decl = parent
+        path.reverse()
+        return path
+
+    def __repr__(self):
+        return ('{0}({1!r}, {2!r}, {3!r}, {4!r})'
+                .format(self.__class__.__name__,
+                        self.name,
+                        self.first_row,
+                        self.last_row,
+                        self.children))
+
+
+class ModuleDecl(Decl):
+    pass
+
+
+class ClassDecl(Decl):
+    pass
+
+
+class FuncDecl(Decl):
+    pass
+
+
+class Visitor(ast.NodeVisitor):
+
+    def __init__(self, lines):
+        self.parent = self.top = ModuleDecl('', 0)
+        self.lines = lines
+        self.last_lineno = 1
+        self.closing_decls = []
+
+    def visitdecl(self, node, cls):
+        decl = cls(node.name, node.lineno - 1)
+        parent = self.parent
+        decl.parent_ref = weakref.ref(parent)
+        parent.children.append(decl)
+        self.last_lineno = node.lineno
+        self.parent = decl
+        self.generic_visit(node)
+        self.parent = parent
+        decl.last_row = self.last_lineno - 1
+        self.closing_decls.append(decl)
+
+    def visit_FunctionDef(self, node):
+        self.close_decls(node.lineno)
+        self.visitdecl(node, FuncDecl)
+
+    def visit_ClassDef(self, node):
+        self.close_decls(node.lineno)
+        self.visitdecl(node, ClassDecl)
+
+    def generic_visit(self, node):
+        if hasattr(node, 'lineno'):
+            self.close_decls(node.lineno)
+            self.last_lineno = max(self.last_lineno, node.lineno)
+        super(Visitor, self).generic_visit(node)
+
+    def close_decls(self, new_lineno):
+        decls = self.closing_decls
+        if decls:
+            # Change the range of the closing declarations to include
+            # multi-line expressions, but not blank lines.
+            # To compute last_row, subtract 1 because the previous
+            # declaration ends on the line before;
+            # subtract 1 again because rows are zero-based while lines are
+            # one-based.
+            last_row = new_lineno - 2
+            while last_row > 0 and last_row < len(self.lines):
+                line = self.lines[last_row]
+                if not line or empty_line_re.match(line):
+                    # Ignore an empty line.
+                    last_row -= 1
+                else:
+                    break
+            for decl in decls:
+                decl.last_row = max(last_row, decl.last_row)
+            del self.closing_decls[:]
+
+
+def list_decls(content, filename):
+    """List the nested declarations in a module."""
+    node = ast.parse(content, filename)
+    lines = content.splitlines()
+    visitor = Visitor(lines)
+    visitor.visit(node)
+    if visitor.closing_decls:
+        visitor.close_decls(len(lines) + 1)
+    return visitor.top.children
+
+
+def find_decl_for_row(decls, row):
+    for decl in decls:
+        if row >= decl.first_row and row <= decl.last_row:
+            child = find_decl_for_row(decl.children, row)
+            if child is not None:
+                return child
+            else:
+                return decl
+    return None
 
 
 class GotoTestCommand(sublime_plugin.TextCommand):
@@ -103,7 +228,7 @@ def show_syntax_error(e):
 
 def list_view_decls(view):
     content = view.substr(sublime.Region(0, view.size()))
-    return codemunge.list_decls(content, view.file_name())
+    return list_decls(content, view.file_name())
 
 
 def to_test_class_name(name):
@@ -178,9 +303,8 @@ def insert_rows(view, row, content, margin=2):
 
 class CodeNavigator(object):
     def __init__(self, target_filename, source_filename, content, source_row):
-        self.source_decls = codemunge.list_decls(content, source_filename)
-        self.source_decl = codemunge.find_decl_for_row(self.source_decls,
-                                                       source_row)
+        self.source_decls = list_decls(content, source_filename)
+        self.source_decl = find_decl_for_row(self.source_decls, source_row)
 
         basename = os.path.basename(source_filename)
         relmodule, _ext = os.path.splitext(basename)
@@ -303,12 +427,12 @@ class TestCodeNavigator(CodeNavigator):
             insert_rows(target_view, 0, content)
 
         try:
-            if isinstance(decls[0], codemunge.ClassDecl):
-                if len(decls) >= 2 and isinstance(decls[1], codemunge.FuncDecl):
+            if isinstance(decls[0], ClassDecl):
+                if len(decls) >= 2 and isinstance(decls[1], FuncDecl):
                     self.goto_method(target_view, decls[0], decls[1])
                 else:
                     self.goto_class(target_view, decls[0])
-            elif isinstance(decls[0], codemunge.FuncDecl):
+            elif isinstance(decls[0], FuncDecl):
                 self.goto_func(target_view, decls[0])
         except SyntaxError as e:
             show_syntax_error(e)
@@ -434,3 +558,59 @@ class {testname}(unittest.TestCase):
 
     def make_method_test(self, template_vars):
         return self.method_test_template.format(**template_vars)
+
+
+def test_list_decls():
+    content = ("if 1:\n"          # row 0
+               " def foo():\n"    # row 1
+               "  pass\n"         # row 2
+               "\n"               # row 3
+               "\n"               # row 4
+               " class bar():\n"  # row 5
+               "  def baz():\n"   # row 6
+               "   def zed():\n"  # row 7
+               "    return [\n"   # row 8
+               "     1]\n"        # row 9
+               "\n"               # row 10
+               " # hi!\n"         # row 11
+               "  stop = True\n"  # row 12
+               "class Y: pass\n"  # row 13
+               "class Z:\n"       # row 14
+               " '''stuff\n"      # row 15
+               "... more '''\n"   # row 16
+               "\n")              # row 17
+    decls = list_decls(content, 'codemunge_test')
+    import pprint
+    pprint.pprint(decls)
+    assert len(decls) == 4
+
+    assert decls[0].name == 'foo'
+    assert len(decls[0].children) == 0
+    assert decls[0].first_row == 1
+    assert decls[0].last_row == 2
+
+    assert decls[1].name == 'bar'
+    assert len(decls[1].children) == 1
+    assert decls[1].first_row == 5
+    assert decls[1].last_row == 12
+    assert decls[1].children[0].name == 'baz'
+    assert decls[1].children[0].first_row == 6
+    assert decls[1].children[0].last_row == 11
+    assert len(decls[1].children[0].children) == 1
+    assert decls[1].children[0].children[0].name == 'zed'
+    assert decls[1].children[0].children[0].first_row == 7
+    assert decls[1].children[0].children[0].last_row == 11
+
+    assert decls[2].name == 'Y'
+    assert len(decls[2].children) == 0
+    assert decls[2].first_row == 13
+    assert decls[2].last_row == 13
+
+    assert decls[3].name == 'Z'
+    assert len(decls[3].children) == 0
+    assert decls[3].first_row == 14
+    assert decls[3].last_row == 16
+
+
+if __name__ == '__main__':
+    test_list_decls()
